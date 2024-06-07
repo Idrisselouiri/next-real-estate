776ff303750b3b66c6df3e6cfa1889a053442528
ayoubtahir <ayoub.tahir.2021@gmail.com>
2024-06-07 16:32:28 +0100

    add property section

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Property from "@/components/Property";
 import Hero from "@/components/header/Hero";
 import Service from "@/components/header/Service";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Service />
+      <Property />
     </main>
   );
 }
--- components/header/Service.jsx
+++ components/header/Service.jsx
@@ -2,32 +2,32 @@ import React from "react";
 
 const Service = () => {
   return (
-    <section class="service" id="service">
-      <div class="container">
-        <p class="section-subtitle">Our Services</p>
+    <section className="service" id="service">
+      <div className="container">
+        <p className="section-subtitle">Our Services</p>
 
-        <h2 class="h2 section-title">Our Main Focus</h2>
+        <h2 className="h2 section-title">Our Main Focus</h2>
 
-        <ul class="service-list">
+        <ul className="service-list">
           <li>
-            <div class="service-card">
-              <div class="card-icon">
+            <div className="service-card">
+              <div className="card-icon">
                 <img
                   src="https://github.com/codewithsadee/homeverse/blob/master/assets/images/service-1.png?raw=true"
                   alt="Service icon"
                 />
               </div>
 
-              <h3 class="h3 card-title">
+              <h3 className="h3 card-title">
                 <a href="#">Buy a home</a>
               </h3>
 
-              <p class="card-text">
+              <p className="card-text">
                 over 1 million+ homes for sale available on the website, we can
                 match you with a house you will want to call home.
               </p>
 
-              <a href="#" class="card-link">
+              <a href="#" className="card-link">
                 <span>Find A Home</span>
 
                 <ion-icon name="arrow-forward-outline"></ion-icon>
@@ -36,24 +36,24 @@ const Service = () => {
           </li>
 
           <li>
-            <div class="service-card">
-              <div class="card-icon">
+            <div className="service-card">
+              <div className="card-icon">
                 <img
                   src="https://github.com/codewithsadee/homeverse/blob/master/assets/images/service-2.png?raw=true"
                   alt="Service icon"
                 />
               </div>
 
-              <h3 class="h3 card-title">
+              <h3 className="h3 card-title">
                 <a href="#">Rent a home</a>
               </h3>
 
-              <p class="card-text">
+              <p className="card-text">
                 over 1 million+ homes for sale available on the website, we can
                 match you with a house you will want to call home.
               </p>
 
-              <a href="#" class="card-link">
+              <a href="#" className="card-link">
                 <span>Find A Home</span>
 
                 <ion-icon name="arrow-forward-outline"></ion-icon>
@@ -62,24 +62,24 @@ const Service = () => {
           </li>
 
           <li>
-            <div class="service-card">
-              <div class="card-icon">
+            <div className="service-card">
+              <div className="card-icon">
                 <img
                   src="https://github.com/codewithsadee/homeverse/blob/master/assets/images/service-3.png?raw=true"
                   alt="Service icon"
                 />
               </div>
 
-              <h3 class="h3 card-title">
+              <h3 className="h3 card-title">
                 <a href="#">Sell a home</a>
               </h3>
 
-              <p class="card-text">
+              <p className="card-text">
                 over 1 million+ homes for sale available on the website, we can
                 match you with a house you will want to call home.
               </p>
 
-              <a href="#" class="card-link">
+              <a href="#" className="card-link">
                 <span>Find A Home</span>
 
                 <ion-icon name="arrow-forward-outline"></ion-icon>
