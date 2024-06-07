85e382cb84185e150346c8bdbcd1dff3445a4b4d
ayoubtahir <ayoub.tahir.2021@gmail.com>
2024-06-07 16:27:46 +0100

    add service section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Hero from "@/components/header/Hero";
+import Service from "@/components/header/Service";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Service />
     </main>
   );
 }
